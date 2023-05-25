327c444eaaa6dd73b81cd9dc8c103b6a8e7f0252
Biruk Jeldu <birukjeldu@gmail.com>
2023-05-25 23:02:17 +0300

    created registeration form

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Kebele_Management_System
 {
-    partial class Form1
+    partial class login_page
     {
         /// <summary>
         /// Required designer variable.
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login_page));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.email = new System.Windows.Forms.TextBox();
             this.password = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.login_btn = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.register_btn = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -56,7 +56,6 @@
             this.label1.Size = new System.Drawing.Size(456, 48);
             this.label1.TabIndex = 1;
             this.label1.Text = "የብሔራዊ መታወቅያ ቅድመ ምዝገባ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
@@ -89,22 +88,21 @@
             this.password.PasswordChar = '*';
             this.password.Size = new System.Drawing.Size(357, 33);
             this.password.TabIndex = 5;
-            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
             // 
-            // button1
+            // login_btn
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(553, 277);
-            this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
-            this.button1.Size = new System.Drawing.Size(357, 46);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Continue";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.login_btn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.login_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.login_btn.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.login_btn.Location = new System.Drawing.Point(553, 277);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.login_btn.Size = new System.Drawing.Size(357, 46);
+            this.login_btn.TabIndex = 6;
+            this.login_btn.Text = "Continue";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
             // 
             // label3
             // 
@@ -128,19 +126,20 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Don\'t Have an Account?";
             // 
-            // button2
+            // register_btn
             // 
-            this.button2.BackColor = System.Drawing.Color.SeaGreen;
-            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(554, 447);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
-            this.button2.Size = new System.Drawing.Size(357, 46);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Register Here";
-            this.button2.UseVisualStyleBackColor = false;
+            this.register_btn.BackColor = System.Drawing.Color.SeaGreen;
+            this.register_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.register_btn.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.register_btn.Location = new System.Drawing.Point(554, 447);
+            this.register_btn.Name = "register_btn";
+            this.register_btn.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.register_btn.Size = new System.Drawing.Size(357, 45);
+            this.register_btn.TabIndex = 9;
+            this.register_btn.Text = "Register Here";
+            this.register_btn.UseVisualStyleBackColor = false;
+            this.register_btn.Click += new System.EventHandler(this.register_btn_Click);
             // 
             // label4
             // 
@@ -150,7 +149,6 @@
             this.label4.Size = new System.Drawing.Size(145, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "© 2023. All Rights Reserved.";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // panel1
             // 
@@ -164,16 +162,6 @@
             this.panel1.Size = new System.Drawing.Size(960, 628);
             this.panel1.TabIndex = 11;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label5.Location = new System.Drawing.Point(551, 134);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 18);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "E-mail:";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -184,26 +172,35 @@
             this.label6.TabIndex = 1;
             this.label6.Text = "Password:";
             // 
-            // Form1
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.Location = new System.Drawing.Point(551, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "E-mail:";
+            // 
+            // login_page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(960, 628);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.register_btn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.login_btn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Welcome To Kebele Management System";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "login_page";
+            this.Text = "Kebele Management System";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -217,10 +214,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox email;
         private System.Windows.Forms.TextBox password;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button login_btn;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button register_btn;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label5;
---
+++ LoginForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kebele_Management_System
+{
+    public partial class login_page : Form
+    {
+        public login_page()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void register_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
