5a81f3773a0b99c37fd1bb5039dc076b50105d05
Biruk Jeldu <birukjeldu@gmail.com>
2023-05-31 23:26:35 +0300

    added if email exist

--- Validator.cs
+++ Validator.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
 
 namespace Kebele_Management_System
 {
+    
     internal class Validator
     {
+        private string connectionString = "server=localhost;uid=root;pwd=root;database=kebele_management_system";
         //isValidName
         internal bool isValidName(string name)
         {
@@ -36,5 +39,79 @@ namespace Kebele_Management_System
             if (regex.IsMatch(email)) return true;
             else return false;
         }
+
+        internal bool PhoneNumberExists(string phoneNumber)
+        {
+            bool exists = false;
+            
+
+            try
+            {
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Check in first table
+                    string query1 = "SELECT COUNT(*) FROM waiting WHERE phone = @PhoneNumber";
+                    MySqlCommand command1 = new MySqlCommand(query1, connection);
+                    command1.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+
+                    int count1 = Convert.ToInt32(command1.ExecuteScalar());
+
+                    // Check in second table
+                    string query2 = "SELECT COUNT(*) FROM users WHERE phone = @PhoneNumber";
+                    MySqlCommand command2 = new MySqlCommand(query2, connection);
+                    command2.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+
+                    int count2 = Convert.ToInt32(command2.ExecuteScalar());
+
+                    exists = (count1 > 0) || (count2 > 0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error checking phone number existence: " + ex.Message);
+            }
+
+            return exists;
+        }
+
+        internal bool EmailExists(string email)
+        {
+            bool exists = false;
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Check in first table
+                    string query1 = "SELECT COUNT(*) FROM waiting WHERE email = @Email";
+                    MySqlCommand command1 = new MySqlCommand(query1, connection);
+                    command1.Parameters.AddWithValue("@Email", email);
+
+                    int count1 = Convert.ToInt32(command1.ExecuteScalar());
+
+                    // Check in second table
+                    string query2 = "SELECT COUNT(*) FROM users WHERE email = @Email";
+                    MySqlCommand command2 = new MySqlCommand(query2, connection);
+                    command2.Parameters.AddWithValue("@Email", email);
+
+                    int count2 = Convert.ToInt32(command2.ExecuteScalar());
+
+                    exists = (count1 > 0) || (count2 > 0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error checking email existence: " + ex.Message);
+            }
+
+            return exists;
+        }
+
+
     }
 }
