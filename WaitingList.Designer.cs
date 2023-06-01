bd858bf7f0d41502b0b0ea73928688084250dd55
Biruk Jeldu <birukjeldu@gmail.com>
2023-06-01 15:19:27 +0300

    some fixes on waiting list

--- WaitingList.Designer.cs
+++ WaitingList.Designer.cs
@@ -28,7 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.approve_btn = new System.Windows.Forms.Button();
+            this.decline_btn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.waitingDataGridView = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waitingDataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.panel1.Controls.Add(this.decline_btn);
+            this.panel1.Controls.Add(this.approve_btn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 61);
+            this.panel1.TabIndex = 1;
+            // 
+            // approve_btn
+            // 
+            this.approve_btn.BackColor = System.Drawing.Color.SeaGreen;
+            this.approve_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.approve_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.approve_btn.Font = new System.Drawing.Font("Roboto Medium", 11F, System.Drawing.FontStyle.Bold);
+            this.approve_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.approve_btn.Location = new System.Drawing.Point(42, 12);
+            this.approve_btn.Name = "approve_btn";
+            this.approve_btn.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.approve_btn.Size = new System.Drawing.Size(108, 36);
+            this.approve_btn.TabIndex = 107;
+            this.approve_btn.Text = "Approve";
+            this.approve_btn.UseVisualStyleBackColor = false;
+            this.approve_btn.Click += new System.EventHandler(this.approve_btn_Click);
+            // 
+            // decline_btn
+            // 
+            this.decline_btn.BackColor = System.Drawing.Color.OrangeRed;
+            this.decline_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.decline_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.decline_btn.Font = new System.Drawing.Font("Roboto Medium", 11F, System.Drawing.FontStyle.Bold);
+            this.decline_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.decline_btn.Location = new System.Drawing.Point(171, 12);
+            this.decline_btn.Name = "decline_btn";
+            this.decline_btn.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.decline_btn.Size = new System.Drawing.Size(108, 36);
+            this.decline_btn.TabIndex = 108;
+            this.decline_btn.Text = "Decline";
+            this.decline_btn.UseVisualStyleBackColor = false;
+            this.decline_btn.Click += new System.EventHandler(this.decline_btn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gold;
+            this.panel2.Controls.Add(this.waitingDataGridView);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 61);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 389);
+            this.panel2.TabIndex = 2;
+            // 
+            // waitingDataGridView
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Roboto Light", 14F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.DodgerBlue;
+            this.waitingDataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.waitingDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.waitingDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.waitingDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.waitingDataGridView.Name = "waitingDataGridView";
+            this.waitingDataGridView.ReadOnly = true;
+            this.waitingDataGridView.Size = new System.Drawing.Size(800, 389);
+            this.waitingDataGridView.TabIndex = 3;
             // 
             // WaitingList
             // 
@@ -36,13 +112,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Yellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "WaitingList";
             this.Text = "WaitingList";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.waitingDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button decline_btn;
+        private System.Windows.Forms.Button approve_btn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView waitingDataGridView;
     }
 }
--- WaitingList.cs
+++ WaitingList.cs
@@ -7,14 +7,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Kebele_Management_System
 {
     public partial class WaitingList : Form
     {
+        string connstring = "server=localhost;uid=root;pwd=root;database=kebele_management_system";
         public WaitingList()
         {
             InitializeComponent();
+            DisplayTableData();
         }
+        private void DisplayTableData()
+        {
+            try
+            {
+                string query = "SELECT * FROM waiting"; // Replace "YourTableName" with the actual name of your table
+
+                using (MySqlConnection connection = new MySqlConnection(connstring))
+                {
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        connection.Open();
+
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            waitingDataGridView.DataSource = dataTable; // dataGridView is the name of your DataGridView control
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void approve_btn_Click(object sender, EventArgs e)
+        {
+            // Check if a single row is selected
+            if (waitingDataGridView.SelectedRows.Count == 1)
+            {
+                // Get the selected row
+                DataGridViewRow selectedRow = waitingDataGridView.SelectedRows[0];
+                DateTime today = DateTime.Today;
+                int year = today.Year;
+                int month = today.Month;
+                int day = today.Day;
+                // Get the values from the selected row
+                string id = selectedRow.Cells["id"].Value.ToString();
+                string firstname = selectedRow.Cells["firstname"].Value.ToString();
+                string fathername = selectedRow.Cells["fathername"].Value.ToString();
+                string grandfathername = selectedRow.Cells["grandfathername"].Value.ToString();
+                string email = selectedRow.Cells["email"].Value.ToString();
+                string phone = selectedRow.Cells["phone"].Value.ToString();
+                string sex = selectedRow.Cells["sex"].Value.ToString();
+                string password = selectedRow.Cells["password"].Value.ToString();
+                string age = selectedRow.Cells["age"].Value.ToString();
+                string bloodtype = selectedRow.Cells["bloodtype"].Value.ToString();
+                string nationality = selectedRow.Cells["nationality"].Value.ToString();
+                string occupation = selectedRow.Cells["occupation"].Value.ToString();
+                string dateofbirth = selectedRow.Cells["dateofbirth"].Value.ToString();
+                string maritalstatus = selectedRow.Cells["maritalstatus"].Value.ToString();
+                string approveddate = day.ToString() + '/' + month.ToString() + '/' + year.ToString();
+                string expirationdate = day.ToString() + '/' + month.ToString() + '/' + (year + 10).ToString();
+                string region = selectedRow.Cells["region"].Value.ToString();
+                string zone = selectedRow.Cells["zone"].Value.ToString();
+                string woreda = selectedRow.Cells["woreda"].Value.ToString();
+                string kebele = selectedRow.Cells["kebele"].Value.ToString();
+                byte[] image = (byte[])selectedRow.Cells["image"].Value;
+
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection(connstring))
+                    {
+                        connection.Open();
+
+                        // Insert the selected data into a new table
+                        string insertQuery = "INSERT INTO users (firstname, fathername, grandfathername, email,birthcardverification,expirationdate,image, phone, sex, password, age, bloodtype, nationality, occupation, dateofbirth, maritalstatus, approveddate, region, zone, woreda, kebele) " +
+                            "VALUES (@id, @firstname, @fathername, @grandfathername, @email,@birthcardverification,@expirationdate,@image, @phone, @sex, @password, @age, @bloodtype, @nationality, @occupation, @dateofbirth, @maritalstatus, @approveddate, @region, @zone, @woreda, @kebele)";
+
+                        using (MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection))
+                        {
+                            //insertCommand.Parameters.AddWithValue("@id", id);
+                            insertCommand.Parameters.AddWithValue("@firstname", firstname);
+                            insertCommand.Parameters.AddWithValue("@fathername", fathername);
+                            insertCommand.Parameters.AddWithValue("@grandfathername", grandfathername);
+                            insertCommand.Parameters.AddWithValue("@email", email);
+                            insertCommand.Parameters.AddWithValue("@phone", phone);
+                            insertCommand.Parameters.AddWithValue("@sex", sex);
+                            insertCommand.Parameters.AddWithValue("@password", password);
+                            insertCommand.Parameters.AddWithValue("@age", age);
+                            insertCommand.Parameters.AddWithValue("@bloodtype", bloodtype);
+                            insertCommand.Parameters.AddWithValue("@nationality", nationality);
+                            insertCommand.Parameters.AddWithValue("@occupation", occupation);
+                            insertCommand.Parameters.AddWithValue("@dateofbirth", dateofbirth);
+                            insertCommand.Parameters.AddWithValue("@maritalstatus", maritalstatus);
+                            insertCommand.Parameters.AddWithValue("@approveddate", approveddate);
+                            insertCommand.Parameters.AddWithValue("@expirationdate", expirationdate);
+                            insertCommand.Parameters.AddWithValue("@region", region);
+                            insertCommand.Parameters.AddWithValue("@zone", zone);
+                            insertCommand.Parameters.AddWithValue("@woreda", woreda);
+                            insertCommand.Parameters.AddWithValue("@kebele", kebele);
+                            insertCommand.Parameters.AddWithValue("@image", image);
+                            insertCommand.Parameters.AddWithValue("@birthcardverification", "false");
+
+                            insertCommand.ExecuteNonQuery();
+                        }
+
+                        // Delete the selected row from the current table
+                        string deleteQuery = "DELETE FROM waiting WHERE id = @id";
+                        using (MySqlCommand deleteCommand = new MySqlCommand(deleteQuery, connection))
+                        {
+                            deleteCommand.Parameters.AddWithValue("@id", id);
+                            deleteCommand.ExecuteNonQuery();
+                        }
+
+                        // Refresh the DataGridView
+                        DisplayTableData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void decline_btn_Click(object sender, EventArgs e)
+        {
+            // Check if a single row is selected
+            if (waitingDataGridView.SelectedRows.Count == 1)
+            {
+                // Get the selected row
+                DataGridViewRow selectedRow = waitingDataGridView.SelectedRows[0];
+
+                // Get the ID from the selected row
+                string id = selectedRow.Cells["id"].Value.ToString();
+
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection(connstring))
+                    {
+                        connection.Open();
+
+                        // Delete the selected row from the current table
+                        string deleteQuery = "DELETE FROM waiting WHERE id = @id";
+                        using (MySqlCommand deleteCommand = new MySqlCommand(deleteQuery, connection))
+                        {
+                            deleteCommand.Parameters.AddWithValue("@id", id);
+                            deleteCommand.ExecuteNonQuery();
+                        }
+
+                        // Refresh the DataGridView
+                        DisplayTableData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
     }
 }
