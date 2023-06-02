ea45462a3151d37c92192efeb7c3939161fcc9e4
Biruk Jeldu <birukjeldu@gmail.com>
2023-06-03 01:39:26 +0300

    login validation

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -31,8 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login_page));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.email = new System.Windows.Forms.TextBox();
-            this.password = new System.Windows.Forms.TextBox();
+            this.email_TB = new System.Windows.Forms.TextBox();
+            this.password_TB = new System.Windows.Forms.TextBox();
             this.login_btn = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,6 +41,8 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.email_error = new System.Windows.Forms.Label();
+            this.pass_error = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -63,35 +65,41 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(488, 628);
+            this.pictureBox1.Size = new System.Drawing.Size(488, 635);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
-            // email
+            // email_TB
             // 
-            this.email.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.email.Font = new System.Drawing.Font("Roboto Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.email.Location = new System.Drawing.Point(553, 155);
-            this.email.MaxLength = 100;
-            this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(357, 33);
-            this.email.TabIndex = 4;
+            this.email_TB.AutoCompleteCustomSource.AddRange(new string[] {
+            "@gmail.com",
+            "@yahoo.com"});
+            this.email_TB.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.email_TB.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.email_TB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.email_TB.Font = new System.Drawing.Font("Roboto Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_TB.Location = new System.Drawing.Point(553, 155);
+            this.email_TB.MaxLength = 100;
+            this.email_TB.Name = "email_TB";
+            this.email_TB.Size = new System.Drawing.Size(357, 33);
+            this.email_TB.TabIndex = 4;
             // 
-            // password
+            // password_TB
             // 
-            this.password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.password.Font = new System.Drawing.Font("Roboto Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.password.Location = new System.Drawing.Point(553, 223);
-            this.password.MaxLength = 100;
-            this.password.Name = "password";
-            this.password.PasswordChar = '*';
-            this.password.Size = new System.Drawing.Size(357, 33);
-            this.password.TabIndex = 5;
+            this.password_TB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.password_TB.Font = new System.Drawing.Font("Roboto Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_TB.Location = new System.Drawing.Point(553, 223);
+            this.password_TB.MaxLength = 100;
+            this.password_TB.Name = "password_TB";
+            this.password_TB.PasswordChar = '*';
+            this.password_TB.Size = new System.Drawing.Size(357, 33);
+            this.password_TB.TabIndex = 5;
             // 
             // login_btn
             // 
             this.login_btn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.login_btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.login_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
             this.login_btn.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.login_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
@@ -129,6 +137,7 @@
             // register_btn
             // 
             this.register_btn.BackColor = System.Drawing.Color.SeaGreen;
+            this.register_btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.register_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
             this.register_btn.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.register_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
@@ -152,14 +161,16 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.pass_error);
+            this.panel1.Controls.Add(this.email_error);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.password);
-            this.panel1.Controls.Add(this.email);
+            this.panel1.Controls.Add(this.password_TB);
+            this.panel1.Controls.Add(this.email_TB);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(960, 628);
+            this.panel1.Size = new System.Drawing.Size(996, 635);
             this.panel1.TabIndex = 11;
             // 
             // label6
@@ -182,12 +193,28 @@
             this.label5.TabIndex = 0;
             this.label5.Text = "E-mail:";
             // 
+            // email_error
+            // 
+            this.email_error.Location = new System.Drawing.Point(729, 129);
+            this.email_error.Name = "email_error";
+            this.email_error.Size = new System.Drawing.Size(182, 23);
+            this.email_error.TabIndex = 6;
+            this.email_error.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pass_error
+            // 
+            this.pass_error.Location = new System.Drawing.Point(732, 207);
+            this.pass_error.Name = "pass_error";
+            this.pass_error.Size = new System.Drawing.Size(179, 13);
+            this.pass_error.TabIndex = 7;
+            this.pass_error.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // login_page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(960, 628);
+            this.ClientSize = new System.Drawing.Size(996, 635);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.register_btn);
             this.Controls.Add(this.label2);
@@ -196,10 +223,12 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
-            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "login_page";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kebele Management System";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -212,8 +241,8 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox email;
-        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox email_TB;
+        private System.Windows.Forms.TextBox password_TB;
         private System.Windows.Forms.Button login_btn;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
@@ -222,6 +251,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label email_error;
+        private System.Windows.Forms.Label pass_error;
     }
 }
 
--- LoginForm.cs
+++ LoginForm.cs
@@ -2,25 +2,47 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Kebele_Management_System
 {
     public partial class login_page : Form
     {
+        Validator v = new Validator();
+        string connstring = "server=localhost;uid=root;pwd=root;database=kebele_management_system";
         public login_page()
         {
             InitializeComponent();
         }
 
-        
+
 
         private void login_btn_Click(object sender, EventArgs e)
         {
+            int userId = Login(email_TB.Text, password_TB.Text);
+            if (userId != -1)
+            {
+                email_error.Text = "";
+                UserDashboard userDashboard = new UserDashboard(userId);
+                userDashboard.Owner = null;
+                userDashboard.Show();
+                this.Hide();
+                
+
+            }
+            else
+            {
+                email_error.Text = "Invalid email or password";
+                email_error.ForeColor = Color.Red;
+            }
+
+
 
         }
 
@@ -29,5 +51,46 @@ namespace Kebele_Management_System
             RegestarationForm reg = new RegestarationForm();
             reg.ShowDialog();
         }
+
+        public int Login(string email, string password)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connstring))
+                {
+                    connection.Open();
+
+                    string query = "SELECT id FROM users WHERE email = @email AND password = @password";
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@email", email);
+                        command.Parameters.AddWithValue("@password", password);
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Login successful
+                                int userId = reader.GetInt32(0); // Get the ID value from the first column
+                                //Console.WriteLine("Login successful. User ID: " + userId);
+                                return userId;
+                            }
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            // Login failed
+            //Console.WriteLine("Login failed. Invalid email or password.");
+
+            return -1;
+        }
+
     }
 }
