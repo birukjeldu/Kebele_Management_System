d350ec7c154bf675a33328d6ac41f36b0ab507f6
Biruk Jeldu <birukjeldu@gmail.com>
2023-06-05 01:08:02 +0300

    add new admin page

--- AdminSetting.Designer.cs
+++ AdminSetting.Designer.cs
@@ -28,21 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.additionalBirthCardInfo_btn = new System.Windows.Forms.Button();
+            this.label21 = new System.Windows.Forms.Label();
+            this.fullName_label = new System.Windows.Forms.Label();
+            this.GGF_Name_TB = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.GGM_name_TB = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Add New Admin";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "List Of Admin";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.additionalBirthCardInfo_btn);
+            this.groupBox1.Controls.Add(this.label21);
+            this.groupBox1.Controls.Add(this.fullName_label);
+            this.groupBox1.Controls.Add(this.GGF_Name_TB);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.GGM_name_TB);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.groupBox1.Location = new System.Drawing.Point(62, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(669, 383);
+            this.groupBox1.TabIndex = 68;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create New Admin";
+            // 
+            // additionalBirthCardInfo_btn
+            // 
+            this.additionalBirthCardInfo_btn.BackColor = System.Drawing.Color.SeaGreen;
+            this.additionalBirthCardInfo_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.additionalBirthCardInfo_btn.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.additionalBirthCardInfo_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.additionalBirthCardInfo_btn.Location = new System.Drawing.Point(209, 317);
+            this.additionalBirthCardInfo_btn.Name = "additionalBirthCardInfo_btn";
+            this.additionalBirthCardInfo_btn.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.additionalBirthCardInfo_btn.Size = new System.Drawing.Size(186, 46);
+            this.additionalBirthCardInfo_btn.TabIndex = 66;
+            this.additionalBirthCardInfo_btn.Text = "Submit";
+            this.additionalBirthCardInfo_btn.UseVisualStyleBackColor = false;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.BackColor = System.Drawing.Color.Transparent;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label21.ForeColor = System.Drawing.Color.Red;
+            this.label21.Location = new System.Drawing.Point(253, 52);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(15, 20);
+            this.label21.TabIndex = 64;
+            this.label21.Text = "*";
+            // 
+            // fullName_label
+            // 
+            this.fullName_label.AutoSize = true;
+            this.fullName_label.BackColor = System.Drawing.Color.Transparent;
+            this.fullName_label.Font = new System.Drawing.Font("Roboto", 11F);
+            this.fullName_label.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.fullName_label.Location = new System.Drawing.Point(180, 52);
+            this.fullName_label.Name = "fullName_label";
+            this.fullName_label.Size = new System.Drawing.Size(80, 18);
+            this.fullName_label.TabIndex = 42;
+            this.fullName_label.Text = "Full Name:";
+            // 
+            // GGF_Name_TB
+            // 
+            this.GGF_Name_TB.Font = new System.Drawing.Font("Roboto", 13F);
+            this.GGF_Name_TB.Location = new System.Drawing.Point(183, 73);
+            this.GGF_Name_TB.Name = "GGF_Name_TB";
+            this.GGF_Name_TB.Size = new System.Drawing.Size(305, 28);
+            this.GGF_Name_TB.TabIndex = 43;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Roboto", 11F);
+            this.label10.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label10.Location = new System.Drawing.Point(180, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 18);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "Email:";
+            // 
+            // GGM_name_TB
+            // 
+            this.GGM_name_TB.Font = new System.Drawing.Font("Roboto", 13F);
+            this.GGM_name_TB.Location = new System.Drawing.Point(183, 148);
+            this.GGM_name_TB.Name = "GGM_name_TB";
+            this.GGM_name_TB.Size = new System.Drawing.Size(305, 28);
+            this.GGM_name_TB.TabIndex = 52;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(228, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 20);
+            this.label7.TabIndex = 57;
+            this.label7.Text = "*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(180, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 18);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Roboto", 13F);
+            this.textBox1.Location = new System.Drawing.Point(183, 227);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 28);
+            this.textBox1.TabIndex = 68;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(252, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 20);
+            this.label2.TabIndex = 70;
+            this.label2.Text = "*";
             // 
             // AdminSetting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminSetting";
             this.Text = "AdminSetting";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button additionalBirthCardInfo_btn;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label fullName_label;
+        private System.Windows.Forms.TextBox GGF_Name_TB;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox GGM_name_TB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
