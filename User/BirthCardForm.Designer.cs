d6b9ff0b2137b74c1d931257aa21f4fe47d44195
Biruk Jeldu <birukjeldu@gmail.com>
2023-06-07 10:38:15 +0300

    folder structure for users

---
+++ User/BirthCardForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Kebele_Management_System
+{
+    partial class BirthCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BirthCardForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GGF_Name = new System.Windows.Forms.TextBox();
+            this.fullName_label = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.GGM_name = new System.Windows.Forms.TextBox();
+            this.label28 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fatherNationality = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.motherNationality = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.additionalBirthCardInfo_btn = new System.Windows.Forms.Button();
+            this.label21 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1160, 82);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(385, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(552, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Federal Democratic Republic of Ethiopia Vital Event Registration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Medium", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(339, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(652, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "በኢትዮጵያ ፌደራላዊ ዴሞክራሲያዊ ሪፐብሊክ የወሳኝ ኩነት ምዝገባ";
+            // 
+            // GGF_Name
+            // 
+            this.GGF_Name.Font = new System.Drawing.Font("Roboto", 13F);
+            this.GGF_Name.Location = new System.Drawing.Point(9, 73);
+            this.GGF_Name.Name = "GGF_Name";
+            this.GGF_Name.Size = new System.Drawing.Size(305, 28);
+            this.GGF_Name.TabIndex = 43;
+            // 
+            // fullName_label
+            // 
+            this.fullName_label.AutoSize = true;
+            this.fullName_label.BackColor = System.Drawing.Color.Transparent;
+            this.fullName_label.Font = new System.Drawing.Font("Roboto", 11F);
+            this.fullName_label.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.fullName_label.Location = new System.Drawing.Point(6, 52);
+            this.fullName_label.Name = "fullName_label";
+            this.fullName_label.Size = new System.Drawing.Size(191, 18);
+            this.fullName_label.TabIndex = 42;
+            this.fullName_label.Text = "Great Grand Father\'s Name:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(205, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 20);
+            this.label7.TabIndex = 57;
+            this.label7.Text = "*";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Roboto", 11F);
+            this.label10.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label10.Location = new System.Drawing.Point(6, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 18);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "Full Mother\'s  Name:";
+            // 
+            // GGM_name
+            // 
+            this.GGM_name.Font = new System.Drawing.Font("Roboto", 13F);
+            this.GGM_name.Location = new System.Drawing.Point(9, 148);
+            this.GGM_name.Name = "GGM_name";
+            this.GGM_name.Size = new System.Drawing.Size(305, 28);
+            this.GGM_name.TabIndex = 52;
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.BackColor = System.Drawing.Color.Transparent;
+            this.label28.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label28.ForeColor = System.Drawing.Color.Red;
+            this.label28.Location = new System.Drawing.Point(490, 52);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(15, 20);
+            this.label28.TabIndex = 60;
+            this.label28.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(347, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 19);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Father\'s Nationality:";
+            // 
+            // fatherNationality
+            // 
+            this.fatherNationality.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.fatherNationality.FormattingEnabled = true;
+            this.fatherNationality.Items.AddRange(new object[] {
+            "Ethiopian",
+            "Kenyan",
+            "Algerian",
+            "American",
+            "Somalian",
+            "Ertirean"});
+            this.fatherNationality.Location = new System.Drawing.Point(351, 73);
+            this.fatherNationality.Name = "fatherNationality";
+            this.fatherNationality.Size = new System.Drawing.Size(186, 28);
+            this.fatherNationality.TabIndex = 58;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(495, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 20);
+            this.label4.TabIndex = 63;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label5.Location = new System.Drawing.Point(347, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 19);
+            this.label5.TabIndex = 62;
+            this.label5.Text = "Mother\'s Nationality:";
+            // 
+            // motherNationality
+            // 
+            this.motherNationality.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.motherNationality.FormattingEnabled = true;
+            this.motherNationality.Items.AddRange(new object[] {
+            "Ethiopian",
+            "Kenyan",
+            "Algerian",
+            "American",
+            "Somalian",
+            "Ertirean"});
+            this.motherNationality.Location = new System.Drawing.Point(351, 148);
+            this.motherNationality.Name = "motherNationality";
+            this.motherNationality.Size = new System.Drawing.Size(186, 28);
+            this.motherNationality.TabIndex = 61;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.additionalBirthCardInfo_btn);
+            this.groupBox1.Controls.Add(this.label21);
+            this.groupBox1.Controls.Add(this.fullName_label);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.GGF_Name);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.motherNationality);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label28);
+            this.groupBox1.Controls.Add(this.GGM_name);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.fatherNationality);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.groupBox1.Location = new System.Drawing.Point(260, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(744, 426);
+            this.groupBox1.TabIndex = 64;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Additional Family Info";
+            // 
+            // additionalBirthCardInfo_btn
+            // 
+            this.additionalBirthCardInfo_btn.BackColor = System.Drawing.Color.SeaGreen;
+            this.additionalBirthCardInfo_btn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.additionalBirthCardInfo_btn.Font = new System.Drawing.Font("Roboto Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.additionalBirthCardInfo_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.additionalBirthCardInfo_btn.Location = new System.Drawing.Point(231, 374);
+            this.additionalBirthCardInfo_btn.Name = "additionalBirthCardInfo_btn";
+            this.additionalBirthCardInfo_btn.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.additionalBirthCardInfo_btn.Size = new System.Drawing.Size(186, 46);
+            this.additionalBirthCardInfo_btn.TabIndex = 66;
+            this.additionalBirthCardInfo_btn.Text = "Submit";
+            this.additionalBirthCardInfo_btn.UseVisualStyleBackColor = false;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.BackColor = System.Drawing.Color.Transparent;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label21.ForeColor = System.Drawing.Color.Red;
+            this.label21.Location = new System.Drawing.Point(192, 52);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(15, 20);
+            this.label21.TabIndex = 64;
+            this.label21.Text = "*";
+            // 
+            // BirthCardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 537);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "BirthCardForm";
+            this.Text = "Birth Card Registeration ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox GGF_Name;
+        private System.Windows.Forms.Label fullName_label;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox GGM_name;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox fatherNationality;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox motherNationality;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Button additionalBirthCardInfo_btn;
+    }
+}
